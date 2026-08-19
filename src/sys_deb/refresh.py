e065235d3eeb91a82aa6323ce102358af976fa25
Sandu Bogdan <bogdanelsandu2011@gmail.com>
2026-08-19 11:15:34 +0200

    refresh: remove apt clean

--- src/sys_deb/refresh.py
+++ src/sys_deb/refresh.py
@@ -11,15 +11,13 @@ async def refresh(args: Namespace):
     console = Console()
 
     with Progress(console=console) as p:
-        task0 = p.add_task("Cleaning unused apt packages", total=4)
+        task0 = p.add_task("Cleaning unused apt packages", total=3)
         task1 = p.add_task("Cleaning old tmpfiles", total=1)
 
         tmpfiles_clean = asyncio.create_task(run(["systemd-tmpfiles", "--clean"], console))
         tmpfiles_clean.add_done_callback(lambda *_, **__: p.advance(task1))
 
         assert 0 == (await run(["apt", "update"], console))[0]
-        p.advance(task0)
-        assert 0 == (await run(["apt", "clean"], console))[0]
         p.advance(task0)
         assert 0 == (await run(["apt", "autoclean"], console))[0]
         p.advance(task0)
